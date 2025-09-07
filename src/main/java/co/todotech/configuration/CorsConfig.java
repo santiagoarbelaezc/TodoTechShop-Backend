9cc314c8742d9fee0fcaed3c84f894b757744580
santiagoarbelaezc <santiago.arbelaezc@uqvirtual.edu.co>
2025-09-07 15:33:14 -0500

    modificando el cors para pruebas

--- src/main/java/co/todotech/configuration/CorsConfig.java
+++ src/main/java/co/todotech/configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://todotechshop.web.app") // URL de Angular
+                .allowedOrigins("https://todotechshop.web.app", "http://localhost:4200") // URL de Angular
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
