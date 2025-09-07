faaf93cca8ac879ccf808457c8532bbdcbb166b8
santiagoarbelaezc <santiago.arbelaezc@uqvirtual.edu.co>
2025-09-07 10:46:53 -0500

    corrigiendo cors

--- src/main/java/co/todotech/configuration/CorsConfig.java
+++ src/main/java/co/todotech/configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://todotechshop.web.app/") // URL de Angular
+                .allowedOrigins("https://todotechshop.web.app") // URL de Angular
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
@@ -22,7 +22,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("https://todotechshop.web.app/");
+        configuration.addAllowedOrigin("https://todotechshop.web.app");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
